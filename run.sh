187d7adbab37575a86f625679100effef7ccd13c
Adithya <77874262+adithya-hub3@users.noreply.github.com>
2022-07-15 06:52:04 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -31,68 +31,64 @@ echo " "
 if [ "$num" == 1 ]; then
     sleep 2
     echo "-> Starting ARP Defender......."
-	if [ $USER = "root" ] ;then
-		systemctl daemon-reload
-		systemctl enable arp.service
-		systemctl start arp.service
-	else
-		echo "This script needs root Permission .. run it as sudo user please .."
-	fi
-	sleep 2
-	echo "-> Successfully Started :) :)"
-	sleep 2
-	echo "-> Scanning the network for attacks......"
-	while :; do
-		if [ -d /opt/APP_arp/arp_warnings ] | [ -d /opt/APP_arp/Logging ];
-		then
-			sleep 1
-			echo ""
-			echo -e "\e[31m--> Warning!! You have been Attacked" 
-			echo -e "\033[0;36m "
-			echo "--> Select an Option You want to see "
-			echo " 1. Warnings "
-			echo " 2. Logging "
-			echo " 3. Exit "
-			read -p "--> Your Option :-  " Option
-			sleep 1 
-			case $Option in 
-			1)  cd /opt/APP_arp/arp_warnings
-			    echo ""
-			    echo "Files inside the Warnings Directory :- "
-			    echo ""
-			    ls
-			    echo ""
-			    sleep 1
-			    read -p "-> Enter file name :  " num1
-			    sleep 2
-			    echo ""
-			    cat $num1 
-			    echo ""
-			    sleep 4 ;;
-			  
-			 
-			2)  cd /opt/APP_arp/Logging
-			    echo "" 
-			    echo "Files inside the Logging Directory :- "
-			    echo ""
-			    ls
-			    echo ""
-			    sleep 1
-			    read -p "--> Enter file name :  " num2
-			    sleep 2
-			    echo ""
-			    cat $num2
-			    echo ""
-			    sleep 4 ;;
+    systemctl daemon-reload
+    systemctl enable arp.service
+    systemctl start arp.service
+	
+    sleep 2
+    echo "-> Successfully Started :) :)"
+    sleep 2
+    echo "-> Scanning the network for attacks......"
+    while :; do
+	if [ -d /opt/APP_arp/arp_warnings ] | [ -d /opt/APP_arp/Logging ];
+	then
+		sleep 1
+		echo ""
+		echo -e "\e[31m--> Warning!! You have been Attacked" 
+		echo -e "\033[0;36m "
+		echo "--> Select an Option You want to see "
+		echo " 1. Warnings "
+		echo " 2. Logging "
+		echo " 3. Exit "
+		read -p "--> Your Option :-  " Option
+		sleep 1 
+		case $Option in 
+		1)  cd /opt/APP_arp/arp_warnings
+		    echo ""
+		    echo "Files inside the Warnings Directory :- "
+		    echo ""
+		    ls
+		    echo ""
+		    sleep 1
+		    read -p "-> Enter file name :  " num1
+		    sleep 2
+		    echo ""
+		    cat $num1 
+		    echo ""
+		    sleep 4 ;;
+			  	 
+		2)  cd /opt/APP_arp/Logging
+		    echo "" 
+		    echo "Files inside the Logging Directory :- "
+		    echo ""
+		    ls
+		    echo ""
+		    sleep 1
+		    read -p "--> Enter file name :  " num2
+		    sleep 2
+		    echo ""
+		    cat $num2
+		    echo ""
+		    sleep 4 ;;
 			    
-			3) exit 
-			   sleep 2 ;;
+		3) exit 
+		    sleep 2 ;;
 			 
-			*) echo " Invalid Input Entered !!" ;;
-			esac
+		*) echo " Invalid Input Entered !!" ;;
+		esac
 			   
-		fi
-	done
+	fi
+    done
 elif [ "$num" == 2 ]; then
 	sleep 1
 	exit
